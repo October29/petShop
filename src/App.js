2a4b356bea9148a2211523719077315f4a03e25d
octoberdesigner <octoberdesigner@proton.me>
2024-07-07 01:19:30 -0500

    FEAT: page404

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import './assets/css/base/base.css';
-import './assets/css/componentes/card.css'
-import Home from './pages/Home';
-import Sobre from './pages/Sobre';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import "./assets/css/base/base.css";
+import "./assets/css/componentes/card.css";
+
+//Pages
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Page404 from "./pages/Page404";
 
 function App() {
   return (
-    <>
-      <Home/>
-      <Sobre/>
-    </>
+    <Router>
+      <Routes>
+        <Route //Page Home
+          path= "/"
+          element={<Home />}
+        />
+        <Route //Page Sobre
+          path= "/Sobre"
+          element= {<Sobre />}
+        />
+
+        <Route // Error HTTP 404 page
+          path= "*"
+          element= {<Page404 />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
